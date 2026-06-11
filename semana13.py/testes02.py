0aca52ddb13b81b7a68c0f7f0feea97ca6d648fe
brunaschwert-rgb <bruna.schwert@amf.edu.br>
2026-06-11 19:42:20 -0300

    Create testes02.py

---
+++ semana13.py/testes02.py
@@ -0,0 +1,9 @@
+
+🔌 Componentes Necessários1 Placa Arduino (Uno, Nano ou similar)
+1 Servo Motor (ex: SG90 para maquetes ou MG996R para portas reais)
+1 Botão (Pulsador/Push-button)
+1 Resistor de 10k Ω (para o botão)Cabos Jumper e 
+1 Protoboard
+🛠️ Esquema de Ligação (Hardware)
+Servo Motor:Fio Vermelho (VCC) → Pino 5V do ArduinoFio Marrom/Preto (GND) → Pino GND do ArduinoFio Laranja/Amarelo (Sinal) → Pino Digital 9 do ArduinoBotão:Uma perna ligada ao 5V.
+A outra perna ligada ao Pino Digital 2 do Arduino E também ao GND através do resistor de 10k Ω (ligação em Pull-Down).
